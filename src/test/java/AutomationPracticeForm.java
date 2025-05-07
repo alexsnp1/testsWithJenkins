c01356369bb2bbb4d1ca042cb57621783fe22ff0
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-05-07 23:52:45 +0700

    updated AutomationPracticeForm.java

--- src/test/java/AutomationPracticeForm.java
+++ src/test/java/AutomationPracticeForm.java
@@ -22,28 +22,50 @@ public class AutomationPracticeForm {
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
+        //убирает рекламу
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
+
+        //просто обычные поля
         $("#firstName").setValue("Alex");
         $("#lastName").setValue("M");
         $("#userEmail").setValue("myemail@mail.com");
+
+        //чек-бокс
         $("#genterWrapper").$(byText("Male")).click();
+        //поле
         $("#userNumber").setValue("79991113399");
+
+        //datetime
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").$(byText("September")).click();
         $(".react-datepicker__year-select").$(byText("2010")).click();
         $(".react-datepicker__month").$(byText("3")).click();
 
+        //поисковая строка, где высвечиваются значения когда пишешь текст
         $("#subjectsInput").setValue("Eng").pressEnter();
+
+        //чекбокс
         $("#hobbiesWrapper").$(byText("Reading")).click();
+
+        //поле
         $("#currentAddress").setValue("Address 1");
+
+        // загрузить картинку
         $("#uploadPicture").uploadFromClasspath("img.png");
+
+        //дропдауны
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
+
+        //дропдауны
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Noida")).click();
+
+        // кликаем на кнопку submit
         $("#submit").click();
 
+        //проверяем значения
         $(".modal-content").shouldHave(text("Student Name Alex M"));
         $(".modal-content").shouldHave(text("Student Email myemail@mail.com"));
         $(".modal-content").shouldHave(text("Gender Male"));
