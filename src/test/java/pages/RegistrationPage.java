7bfeef12a5088c1a0cc20182729df1b42ad99938
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-05-18 13:41:50 +0700

    added RegistrationPage.java

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,132 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import pages.components.CalendarComponent;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationPage {
+    private SelenideElement firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            emailInput = $("#userEmail"),
+            genderWrapper = $("#genterWrapper"),
+            userNumberInput = $("#userNumber"),
+            calendarInput = $("#dateOfBirthInput"),
+            subjectsInput = $("#subjectsInput"),
+            hobbiesWrapper = $("#hobbiesWrapper"),
+            currentAddress = $("#currentAddress"),
+            uploadPicture = $("#uploadPicture"),
+            state= $("#state"),
+            stateCityWrapper = $("#stateCity-wrapper"),
+            city= $("#city"),
+            submitButton= $("#submit");
+
+
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+
+
+    public RegistrationPage openPage() {
+
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        return this;
+    }
+
+
+    public RegistrationPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setLastName(String value) {
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setEmail(String value) {
+        emailInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setGender(String value) {
+        genderWrapper.$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setUserNumber(String value) {
+        userNumberInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setDateOfBirth(String day, String month, String year) {
+        calendarInput.click();
+        calendarComponent.setDate(day, month, year);
+
+
+        return this;
+    }
+
+    public RegistrationPage setSubjectsInput(String value) {
+        subjectsInput.setValue(value).pressEnter();
+
+        return this;
+    }
+    public RegistrationPage setHobbiesWrapper(String value) {
+        hobbiesWrapper.$(byText(value)).click();
+
+        return this;
+    }
+    public RegistrationPage setCurrentAddress(String value) {
+        currentAddress.setValue(value);
+
+        return this;
+    }
+    public RegistrationPage setPicture(String value) {
+        uploadPicture.uploadFromClasspath(value);
+
+        return this;
+    }
+
+    public RegistrationPage setState() {
+        state.click();
+
+        return this;
+    }
+
+    public RegistrationPage setStateCityWrapper(String value) {
+        stateCityWrapper.$(byText(value)).click();
+
+        return this;
+    }
+    public RegistrationPage setCity() {
+        city.click();
+
+        return this;
+    }
+
+    public RegistrationPage clickSubmitButton() {
+        submitButton.click();
+
+        return this;
+    }
+
+    public RegistrationPage checkResults(String key, String value) {
+        $(".modal-content").$(byText(key)).parent()
+                .shouldHave(text(value));
+        //нашел ключ, вышел наверх на 1 элемент и проверил что есть значение
+
+        return this;
+    }
+
+}
