9846c5bc6566cce870360d5e8a8d4fbadeb19801
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-05-19 00:27:53 +0700

    added ResultsComponent.java

---
+++ src/test/java/pages/components/ResultsComponent.java
@@ -0,0 +1,16 @@
+package pages.components;
+
+import pages.RegistrationPage;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class ResultsComponent {
+
+        public void check(String key, String value) {
+            $(".modal-content").$(byText(key)).parent()
+                    .shouldHave(text(value));
+
+    }
+}
