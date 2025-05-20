5e3b2d7d44a3971014c130bbd2f50c98089f5c16
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-05-20 10:58:35 +0700

    Updated RegistrationWithTestDataTests

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -11,4 +11,13 @@ public class TestData {
                 lastName = "May",
                 lastName = "May",
                 lastName = "May",
+
+    public static String getRandomGender() {
+        String[] genders = {"Male", "Female", "Other"};
+        return getRandomItemFromArray (genders);
+    }
+    public static String getRandomItemFromArray (String[] array) {
+        int index = getRandomInt(0, array.length - 1);
+
+        return array[index];
 }
