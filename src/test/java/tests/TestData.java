16c78f414090661b568198252407cbe9155d421e
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-05-20 10:54:53 +0700

    Added RegistrationWithTestDataTests

---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,14 @@
+package tests;
+
+public class TestData {
+        public static String
+                firstName = "Alex",
+                lastName = "May",
+                userEmail = "email@mail.com",
+                userGender = {"Male", "Female", "Other"},
+                userPhone = "79991113399",
+                lastName = "May",
+                lastName = "May",
+                lastName = "May",
+                lastName = "May",
+}
