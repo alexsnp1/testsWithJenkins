f2182a1576e94bdade78df1ead8168895e6c87e4
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-05-20 11:40:43 +0700

    Updated TestData

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -1,23 +1,50 @@
 package tests;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class TestData {
-        public static String
-                firstName = "Alex",
-                lastName = "May",
-                userEmail = "email@mail.com",
-                userGender = {"Male", "Female", "Other"},
-                userPhone = "79991113399",
-                lastName = "May",
-                lastName = "May",
-                lastName = "May",
-                lastName = "May",
+    public static void main(String[] args) {
+        System.out.println(firstName);
+        System.out.println(userGender);
+        System.out.println(userSubjects);
+        System.out.println(userHobbies);
+
+    }
+
+    public static String
+            firstName = "Alex",
+            lastName = "May",
+            userEmail = "myemail",
+            userGender = getRandomGender(),
+            userPhone = "79991113399",
+            userSubjects = getRandomSubject(),
+            userHobbies = getRandomHobbies();
+
+    public static int getRandomInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+
+    }
 
     public static String getRandomGender() {
         String[] genders = {"Male", "Female", "Other"};
-        return getRandomItemFromArray (genders);
+        return getRandomItemFromArray(genders);
     }
-    public static String getRandomItemFromArray (String[] array) {
+
+    public static String getRandomItemFromArray(String[] array) {
         int index = getRandomInt(0, array.length - 1);
 
         return array[index];
+    }
+    public static String getRandomSubject() {
+        String[] subjects = {"Eng", "Che", "Comm", "Eco", "Soc"};
+        return getRandomItemFromArray(subjects);
+    }
+    public static String getRandomHobbies() {
+        String[] hobbies = {"Reading", "Sports", "Music"};
+        return getRandomItemFromArray(hobbies);
+    }
+
+
+
+
 }
