ee1f328edbd0d6df4e4677dbac453b8b7d04212f
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-07-13 22:09:00 +0700

    added screenshot attach

--- src/test/java/tests/AutomationPracticeFormRemote.java
+++ src/test/java/tests/AutomationPracticeFormRemote.java
@@ -2,7 +2,9 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -27,6 +29,10 @@ public class AutomationPracticeFormRemote {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last Screenshot");
+    }
     @Test
     @Tag("demoqa")
     void fillFormTest() {
